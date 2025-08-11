f572d0fe2e8cc84706f6f7aef6894534a20e53f6
Marcus Vinicius <marcusvinicius.rj45@gmail.com>
2025-08-11 18:29:43 -0300

    Update scriptaula13_exercicio.js

---
+++ scriptaula13_exercicio.js
@@ -0,0 +1,22 @@
+// Variaveis da pagina
+function carregar() {
+  var msg = window.document.getElementaryById('msg')
+  var img = window.documentgetElementaryById('imagem')
+  var data = new Date()
+  var hora + data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas!`
+
+  if (hora >=0 && <12) {
+    // Bom dia! // Goog Morning! // Guten Mogen!
+        img.src='manha-removebg-preview.png'
+        document.body.style.backgroud = '#'
+  } else if (hora >= 12 && hora <=18) {
+    // Boa Tarde! // Good Afternoon! // Guten Tagen!
+        img.src='tarde-removebg-preview.png'
+        document.body.style.backgroud = '#'
+  } else {
+    // Boa Noite! // Good Evening! // Guten Abend!
+        img.src='noite-removebg-preview.png'
+        document.body.style.backgroud = '#'
+  }
+} 
